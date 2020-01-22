315980d0c5769ece8ec74fb0fa1aa6edc18649c2
VictorMunayco96 <m.m.victor9630@gmail.com>
2020-01-22 16:02:34 -0500

    aaa

---
+++ Config/Global.php
@@ -0,0 +1,9 @@
+<?php 
+
+define("DB_HOST","192.168.104.124");
+define("DB_NAME","DBMolino");
+define("DB_USERNAME","Visit");
+define("DB_PASSWORD","Paco123");
+define("DB_ENCODE","utf8");
+define("PRO_NOMBRE", "CALERA - MOLINO");
+?>
